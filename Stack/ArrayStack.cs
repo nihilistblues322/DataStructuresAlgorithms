4de81bb41de1e90f7c15a15590efb40258123520
AD <miserylovescompany228@gmail.com>
2025-04-19 13:21:09 +0500

    array stack

---
+++ Stack/ArrayStack.cs
@@ -0,0 +1,75 @@
+﻿namespace Stack;
+
+public class ArrayStack
+{
+    private int[] _items;
+    private int _top;
+    private int _capacity;
+
+    public ArrayStack(int capacity = 10)
+    {
+        _capacity = capacity;
+        _items = new int[capacity];
+        _top = -1;
+    }
+
+    public void Push(int value)
+    {
+        if (_top == _capacity - 1)
+        {
+            _capacity *= 2;
+            Array.Resize(ref _items, _capacity);
+        }
+
+        _top++;
+        _items[_top] = value;
+    }
+
+    public int? Pop()
+    {
+        if (IsEmpty())
+            return null;
+
+        int value = _items[_top];
+        _top--;
+
+        return value;
+    }
+
+    public bool IsEmpty()
+    {
+        return _top == -1;
+    }
+
+    public int Peek()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Stack is empty");
+
+        return _items[_top];
+    }
+
+    public void PrintVisual()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Stack is empty");
+            return;
+        }
+
+        Console.WriteLine("\nStack Visualization (TOP to BOTTOM):");
+        Console.WriteLine("-------------------");
+        Console.WriteLine("  TOP");
+
+        for (int i = _top; i >= 0; i--)
+        {
+            Console.WriteLine("+-----+");
+            Console.WriteLine($"| {_items[i],3} |");
+        }
+
+        Console.WriteLine("+-----+");
+        Console.WriteLine("  BOTTOM");
+        Console.WriteLine($"\nStack size: {_top + 1}\n");
+        Console.WriteLine($"Current capacity: {_capacity}\n");
+    }
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -7,12 +7,22 @@ class Program
         Stack myStack = new Stack(0);
         
         myStack.Push(1);
+        myStack.Push(2);
         myStack.Push(3);
         myStack.Push(4);
         myStack.Push(5);
-        myStack.Push(6);
+        myStack.Pop();
         myStack.Pop();
         
         myStack.PrintVisual();
+        
+        
+        ArrayStack myArrayStack = new ArrayStack(10);
+        myArrayStack.Push(100);
+        myArrayStack.Push(200);
+        myArrayStack.Push(300);
+        myArrayStack.Push(400);
+        
+        myArrayStack.PrintVisual();
     }
 }
