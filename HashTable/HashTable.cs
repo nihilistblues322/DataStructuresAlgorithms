1e290216135e5bcda028f178ed64ac3e48f352bf
dimash.abdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-04-26 18:47:08 +0500

    hash table get

--- HashTable/HashTable.cs
+++ HashTable/HashTable.cs
@@ -5,12 +5,25 @@ public class HashTable
     private int _size = 7;
     private Node?[] _dataMap;
 
+    private const int PrimeMultiplier = 23;
+    private static int _count = 0;
+
     public HashTable()
     {
         _dataMap = new Node[_size];
     }
 
     private int Hash(string key)
+    {
+        unchecked
+        {
+            return key
+                .Aggregate(0,
+                    (currentHash, character) => (currentHash + character * PrimeMultiplier) % _dataMap.Length);
+        }
+    }
+
+    private int OldHash(string key)
     {
         int hash = 0;
 
@@ -27,6 +40,92 @@ public class HashTable
 
     public void Set(string key, int value)
     {
+        if (string.IsNullOrWhiteSpace(key))
+            throw new ArgumentException("Key cannot be null, empty, or whitespace.", nameof(key));
+
+        int index = Hash(key);
+
+        Node newNode = new Node(key, value);
+
+        if (_dataMap[index] == null)
+        {
+            _dataMap[index] = newNode;
+            _count++;
+        }
+        else
+        {
+            Node current = _dataMap[index]!;
+
+            while (true)
+            {
+                if (string.Equals(current.Key, key, StringComparison.Ordinal))
+                {
+                    current.Value = value;
+                    return;
+                }
+
+                if (current.Next == null)
+                {
+                    current.Next = newNode;
+                    _count++;
+                    return;
+                }
+
+                current = current.Next;
+            }
+        }
+
+        if ((double)_count / _size > 0.7)
+        {
+            Console.WriteLine("\nResize table, LoadFactor > 70%");
+            Resize();
+        }
+    }
+
+    public int Get(string key)
+    {
+        int index = Hash(key);
+
+        Node? current = _dataMap[index];
+
+        while (current != null)
+        {
+            if (string.Equals(current.Key, key, StringComparison.Ordinal))
+            {
+                Console.WriteLine($" Found key '{key}' with Value = {current.Value}");
+                return current.Value;
+            }
+
+            current = current.Next;
+        }
+
+        return 0;
+    }
+
+    private void Resize()
+    {
+        int newSize = _size * 2;
+        Node?[] newDataMap = new Node[newSize];
+
+        Node?[] oldDataMap = _dataMap;
+
+
+        _dataMap = newDataMap;
+        _size = newSize;
+
+        foreach (var node in oldDataMap)
+        {
+            Node? current = node;
+            while (current != null)
+            {
+                RehashAndInsert(current.Key, current.Value);
+                current = current.Next;
+            }
+        }
+    }
+
+    private void RehashAndInsert(string key, int value)
+    {
         int index = Hash(key);
 
         Node newNode = new Node(key, value);
@@ -37,17 +136,18 @@ public class HashTable
         }
         else
         {
-            Node temp = _dataMap[index]!;
+            Node current = _dataMap[index]!;
 
-            while (temp.Next != null)
+            while (current.Next != null)
             {
-                temp = temp.Next;
+                current = current.Next;
             }
 
-            temp.Next = newNode;
+            current.Next = newNode;
         }
     }
-    
+
+
     public void PrintTable()
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -77,13 +177,14 @@ public class HashTable
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.Write($"{current.Value}");
                 Console.ResetColor();
-                
+
                 current = current.Next;
                 if (current != null) Console.Write(" ║ ");
             }
+
             Console.WriteLine();
         }
-        
+
         Console.ForegroundColor = ConsoleColor.DarkYellow;
         Console.WriteLine("➾ Size: " + _size);
         Console.ResetColor();
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -5,16 +5,23 @@ class Program
     static void Main(string[] args)
     {
         HashTable hashTable = new HashTable();
-        
-        hashTable.Set("nails", 100);
-        hashTable.Set("coffees", 100);
-        hashTable.Set("milk", 100);
-        hashTable.Set("bolts", 100);
-        hashTable.Set("screws", 100);
-        
+
+        hashTable.Set("milk", 87);
+        hashTable.Set("coffees", 6);
+        hashTable.Set("milk", 22);
+        hashTable.Set("bolts", 64);
+        hashTable.Set("screws", 54);
+        hashTable.Set("pizza", 321);
+        hashTable.Set("potato", 322);
+        hashTable.Set("carrots", 77);
+        hashTable.Set("qwe", 876);
+        hashTable.Set("asd", 765);
+        hashTable.Set("bacon", 786);
+
+
+        hashTable.Get("milk");
+        hashTable.Get("potato");
+
         hashTable.PrintTable();
-        
-        
-        
     }
 }
