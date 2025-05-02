580b01615dc133084fdb36980842b5a403332a91
dimash.abdrakhmanov <dimash.abdrakhmanov@brokeree.com>
2025-05-03 00:03:04 +0500

    factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Factorial;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int fact = Factorial(4);
+        Console.WriteLine(fact);
+    }
+
+    private static int Factorial(int n)
+    {
+        if (n == 1) return 1;
+
+        return n * Factorial(n - 1);
+    }
+}
